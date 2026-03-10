d9080be411954ab4a047e2f9019889bc45aaac29
duyhuynh <duyhuynh9393@gmail.com>
2026-03-10 11:13:16 +0700

    first commit

---
+++ L1_compilerandmacro/code_compiler/define_example.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define STRINGSIZE(x) #x
+#define DATA 40
+
+// Toán tử #
+int main(){
+    printf("%s ", STRINGSIZE(DATA));
+}
+
+
---
+++ L4_Variadic macro/Variadic_sum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+#define sum(...)                                               \
+int arr[] = {__VA_ARGS__};                                     \
+int result = 0;                                                \
+for (int i = 0; i < (sizeof(arr[]) / sizeof(arr[0])); i++){    \                                                             \
+    result += arr[i];                                          \
+}                                                              \
+printf("Sum = %d\n", result);
+
+int main()
+{
+    sum(,1,2,3);
+    return 0;
+}
